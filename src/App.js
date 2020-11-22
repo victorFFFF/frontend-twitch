b665f49fa3a2162494bb06986860132270d06823
Victor Li <victorisnice@Victors-MBP-2.home>
2020-11-22 09:13:19 -0500

    progress made from class to hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TopGameView from "./components/TopGameView";
 import SearchView from "./components/SearchView";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      oAuth: "",
-      topGames: [],
-      searchInput: "",
-      searchedGames: [],
-      gameImage: [],
-    };
-    this.updateTopGame = this.updateTopGame.bind(this);
-    this.updateSearch = this.updateSearch.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.getOAuth = this.getOAuth.bind(this);
-    this.updateTopGameImage = this.updateTopGameImage.bind(this);
-  }
+function App() {
+  const [topGames, setTopGames] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchedGames, setSearchedGames] = useState([]);
+  const [gameImage, setGameImage] = useState([]);
+  const [oAuth, setoAut] = useState("");
 
   //Get new OAuthenitcation key
-  getOAuth() {
-    axios.get("oAuth").then((response) => {
-      const data = response.data;
-      this.setState({ oAuth: data });
-      console.log(data);
-      console.log("oAuth : " + this.oAuth);
-    });
-  }
+  const getOAuth = () => {
+    // axios.get("oAuth").then((response) => {
+    //   const data = response.data;
+    //   // this.setState({ oAuth: data });
+    //   console.log(data);
+    //   console.log("oAuth : " + this.oAuth);
+    // });
+  };
 
   //Update top game
-  updateTopGame() {
-    this.setState({ topGames: [], gameImage: [] });
+  const updateTopGame = () => {
+    setTopGames([]);
+    // setGameImage([...gameImage, [] ]);
     axios
       .get("https://api.twitch.tv/helix/games/top", {
         headers: {
@@ -43,47 +33,43 @@ class App extends Component {
       })
       .then((response) => {
         const result = response.data;
-        console.log(result.data);
+        console.log(result.data.length);
         for (let i = 0; i < result.data.length; i++) {
-          this.setState({
-            topGames: [...this.state.topGames, result.data[i].name],
-          });
+          setTopGames((prevState) => [...prevState, result.data[i].name]);
         }
       });
-  }
+  };
 
   //Update image
-  updateTopGameImage() {
-    console.log("TOP");
-    console.log(this.state.topGames);
-    for (let i = 0; i < 20; i++) {
-      axios
-        .get(
-          `https://api.twitch.tv/helix/search/categories?query=${this.state.topGames[i]}`,
-          {
-            headers: {
-              "Client-ID": `${process.env.REACT_APP_CLIENTID}`,
-              Authorization: `Bearer ${process.env.REACT_APP_OAUTHTOKEN}`,
-            },
-          }
-        )
-        .then((response) => {
-          const result = response.data;
-          this.setState({
-            gameImage: [...this.state.gameImage, result.data[0].box_art_url],
-          });
-        });
-    }
-  }
+  const updateTopGameImage = () => {
+    // console.log(this.state.topGames);
+    // for (let i = 0; i < 20; i++) {
+    //   axios
+    //     .get(
+    //       `https://api.twitch.tv/helix/search/categories?query=${this.state.topGames[i]}`,
+    //       {
+    //         headers: {
+    //           "Client-ID": `${process.env.REACT_APP_CLIENTID}`,
+    //           Authorization: `Bearer ${process.env.REACT_APP_OAUTHTOKEN}`,
+    //         },
+    //       }
+    //     )
+    //     .then((response) => {
+    //       const result = response.data;
+    //       this.setState({
+    //         gameImage: [...this.state.gameImage, result.data[0].box_art_url],
+    //       });
+    //     });
+    // }
+  };
 
   //search for games matching input
-  updateSearch(e) {
+  const updateSearch = (e) => {
     e.preventDefault();
-    this.setState({ searchedGames: [] });
+    setSearchedGames([]);
     axios
       .get(
-        "https://api.twitch.tv/helix/search/categories?query=" +
-          this.state.searchInput,
+        "https://api.twitch.tv/helix/search/categories?query=" + searchInput,
         {
           headers: {
             "Client-ID": `${process.env.REACT_APP_CLIENTID}`,
@@ -93,47 +79,37 @@ class App extends Component {
       )
       .then((response) => {
         const result = response.data;
+        console.log(result);
         for (let i = 0; i < result.data.length; i++) {
-          this.setState({
-            searchedGames: [
-              ...this.state.searchedGames,
-              result.data[i].box_art_url,
-            ],
-          });
+          setSearchedGames((prevState) => [...prevState, result.data[i].name]);
         }
       });
-  }
-
-  handleInputChange(e) {
-    this.setState({ searchInput: e.target.value });
-  }
-
-  componentWillMount = () => {
-    this.updateTopGame();
   };
 
-  componentDidMount = () => {
-    this.updateTopGameImage();
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
   };
 
-  render() {
-    // console.log(this.state.gameImage);
-    return (
-      <div>
-        <SearchView
-          searchedGames={this.state.searchedGames}
-          updateSearch={this.updateSearch}
-          handleInputChange={this.handleInputChange}
-        />
-        <TopGameView
-          topGames={this.state.topGames}
-          updateTopGame={this.updateTopGame}
-          getOAuth={this.getOAuth}
-          gameImage={this.state.gameImage}
-        />
-      </div>
-    );
-  }
+  useEffect(() => {
+    updateTopGame();
+  }, []);
+
+  // console.log(this.state.gameImage);
+  return (
+    <div>
+      <SearchView
+        searchedGames={searchedGames}
+        updateSearch={updateSearch}
+        handleInputChange={handleInputChange}
+      />
+      <TopGameView
+        topGames={topGames}
+        updateTopGame={updateTopGame}
+        getOAuth={getOAuth}
+        gameImage={gameImage}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,29 +1,29 @@
+import React, { useState } from "react";
 
-import React, {Component} from 'react';
+function SearchView({ handleInputChange, updateSearch, searchedGames }) {
+  return (
+    <div>
+      <form className="form" id="addItemForm">
+        <input
+          type="text"
+          className="input"
+          placeholder="Search Games"
+          onChange={handleInputChange}
+        />
+        <button className="button is-info" onClick={updateSearch}>
+          Search
+        </button>
+      </form>
 
-
-class SearchView extends Component {    
-
-    render(){
-        return (
-            <div>
-            <form className="form" id="addItemForm">
-            <input type="text" className="input" placeholder="Search Games" onChange={this.props.handleInputChange}/>
-            <button className="button is-info" onClick={this.props.updateSearch}>
-              Search
-            </button>
-          </form>
-        
-            <h1>Top Search Result</h1>
-            {this.props.searchedGames.map((name,i) =>
-            <ol key={i}> {i+1} {name}</ol>)}
-            
+      <h1>Top Search Result</h1>
+      {searchedGames.map((name, i) => (
+        <ol key={i}>
+          {" "}
+          {i + 1} {name}
+        </ol>
+      ))}
     </div>
-
-      
-        );
-        }
-
+  );
 }
 
 export default SearchView;
