68db729f517d949731dd6a1f5d07cec80683b9f0
Victor Li <victor.li.1995@hotmail.com>
2020-12-24 22:33:42 -0500

    fix cor issue

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,26 @@ import axios from "axios";
 import Button from "react-bootstrap/Button";
 
 export default function Login() {
+  const [userLogin, setUser] = useState("");
+  const [passwordLogin, setPassword] = useState("");
+
+  const inputUser = (e) => {
+    e.preventDefault();
+    setUser(e.target.value);
+  };
+
+  const inputPW = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  console.log("USER " + userLogin);
+  console.log("Password " + passwordLogin);
   return (
     <div className="centerMiddle3">
-      User: <input placeholder="UserName" />
+      User: <input placeholder="UserName" onChange={inputUser} />
       <br></br>
-      Password: <input placeholder="Password" />
+      Password: <input placeholder="Password" onChange={inputPW} />
       <button>Login</button>
       <br></br>
       <br></br>
--- src/components/Register.js
+++ src/components/Register.js
@@ -5,13 +5,67 @@ import axios from "axios";
 import Button from "react-bootstrap/Button";
 
 export default function Register() {
+  const [userLogin, setUser] = useState("");
+  const [passwordLogin, setPassword] = useState("");
+  const [passwordLoginVerify, setPasswordVerify] = useState("");
+
+  const inputUser = (e) => {
+    e.preventDefault();
+    setUser(e.target.value);
+  };
+
+  const inputPW = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const inputPWVerify = (e) => {
+    e.preventDefault();
+    setPasswordVerify(e.target.value);
+  };
+
+  const register = () => {
+    if (passwordLogin !== passwordLoginVerify) {
+      console.log("password has to match");
+    } else {
+      axios
+        .post(
+          "http://localhost:3001/User/register/",
+          { userLogin, passwordLogin },
+          {
+            headers: {
+              withCredentials: true,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <div className="centerMiddle3">
-      User: <input placeholder="UserName" />
+      User: <input placeholder="UserName" onChange={inputUser} />
       <br></br>
-      Password: <input placeholder="Password" />
-      Verifiy Password: <input placeholder="Verify password" />
-      <button>Register</button>
+      Password:{" "}
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={inputPW}
+      />
+      Verifiy Password:{" "}
+      <input
+        type="password"
+        name="password"
+        placeholder="Verify password"
+        onChange={inputPWVerify}
+      />
+      <button onClick={register}>Register</button>
       <br></br>
       <br></br>
       Already a member?
